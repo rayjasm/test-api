a11915f62b1b6cc86c77ce63066f200f51eb87ca
rei hori <rei@reinoMacBook-Air.local>
2023-11-11 19:47:06 +0900

    aa

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+import json
+
+app = FastAPI()
+
+class req(BaseModel):
+    image_path: str
+
+@app.post('/item/')
+def post_test(item: req):
+    return {
+    "success": "true",
+    "message": "success",
+    "estimated_data": {
+    "class": 3,
+    "confidence": 0.8683
+    }
+    }
